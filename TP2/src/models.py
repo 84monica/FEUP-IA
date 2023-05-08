85cdbc377e4da887899be1baed55d541c8e21421
84monica <up201905753@fe.up.pt>
2023-05-08 16:06:13 +0100

    Add more evaluation metrics

--- TP2/src/models.py
+++ TP2/src/models.py
@@ -18,10 +18,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 # --------------------- SMOTE --------------------- #
 
-print(Counter(y_train))
 smote = SMOTE(random_state = 2)
 X_train, y_train = smote.fit_resample(X_train, y_train)
-print(Counter(y_train))
 
 # --------------------- DECISION TREE --------------------- #
 
@@ -33,7 +31,9 @@ y_pred = model_dt.predict(X_test)
 # Evaluate the model
 print("Accuracy: ", model_dt.score(X_test, y_test))
 print("Precision: ", metrics.precision_score(y_test, y_pred))
+print("Recall: ", metrics.recall_score(y_test, y_pred))
+print("F1 Score: ", metrics.f1_score(y_test, y_pred))
+print("Confusion Matrix:\n", pd.crosstab(y_test, y_pred, rownames=['Actual'], colnames=['Predicted']))
 
 # --------------------- TODO LIST --------------------- #
-# - More Evaluation metrics
 # - More models
