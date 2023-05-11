0b63d470a8467a3abfe3b2b236b3f11222736918
84monica <up201905753@fe.up.pt>
2023-05-11 17:26:45 +0100

    Removing Features with low correlation with target variable Heart Disease

--- TP2/src/pre_processing.py
+++ TP2/src/pre_processing.py
@@ -6,19 +6,14 @@ dataset = pd.read_csv('../dataset/Heart_Disease.csv')
 
 # --------------------- CLEAN DATASET --------------------- #
 
-# check and remove duplicates
+# Check and remove duplicates
 duplicate_mask = dataset.duplicated()
 heart_disease = dataset.drop_duplicates()
 
 # --------------------- HANDLING CATEGORICAL FEATURES --------------------- #
 
-# For now removing age category
-print(dataset['AgeCategory'].unique())
-print(dataset['AgeCategory'].value_counts())
-dataset = dataset.drop(['AgeCategory'], axis=1)
-
 # One-Hot Encoding for categorical features
-dataset = pd.get_dummies(dataset, columns = ["Smoking","AlcoholDrinking","Stroke","DiffWalking","Sex","Race","Diabetic","PhysicalActivity","GenHealth","Asthma","KidneyDisease","SkinCancer"])
+dataset = pd.get_dummies(dataset, columns = ["Race", "AgeCategory","Smoking","AlcoholDrinking","Stroke","DiffWalking","Sex","Diabetic","PhysicalActivity","GenHealth","Asthma","KidneyDisease","SkinCancer"])
 
 # Change target variable to binary values
 d = {'Yes': 1, 'No': 0}
@@ -26,25 +21,19 @@ dataset['HeartDisease'] = dataset['HeartDisease'].map(d)
 
 # --------------------- CORRELATION MATRIX --------------------- #
 
+# Correlation matrix only with target variable
 corr_matrix = dataset.corr(method='spearman')
-print(corr_matrix)
-
-# Plot the correlation matrix using seaborn heatmap function
-plt.figure(figsize=(12, 10))
-sns.heatmap(corr_matrix, annot=True)
-plt.title('Correlation Matrix')
-plt.show()
+corr_matrix = corr_matrix.loc[['HeartDisease']]
+corr_matrix = corr_matrix.drop(['HeartDisease'], axis=1)
 
 # --------------------- FEATURE SELECTION --------------------- #
 
-# Drop race column
-dataset = dataset.drop(['Race'], axis=1)
+# Drop features with low correlation
+treshold = 0.12
+corr_matrix = corr_matrix.loc[:, (abs(corr_matrix) > treshold).any()]
+features_corr = list(corr_matrix.columns)
+dataset = dataset[['HeartDisease'] + features_corr]
+print(features_corr)
 
 # Save dataset
-print(dataset)
 dataset.to_csv('../dataset/Heart Disease - Processed.csv', index=False)
-
-# --------------------- TODO LIST --------------------- #
-# TODO: Target Encoding because many age categories could be a problem
-# TODO: Clean correlation matrix
-# TODO: Feature selection
