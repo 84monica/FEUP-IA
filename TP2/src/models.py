30e090fdadc5b43d9f0ae0fae91bf0b9dba88e9e
Mónica Araújo <up202005209@up.pt>
2023-05-18 01:50:40 +0100

    modify the parameters for f1 score

--- TP2/src/models.py
+++ TP2/src/models.py
@@ -92,7 +92,7 @@ param_grid = {
 grid_search = GridSearchCV(
     estimator=grid_model_dt,
     param_grid=param_grid,
-    scoring='accuracy',
+    scoring="f1",
     cv=5,
     n_jobs=-1
 )
@@ -110,9 +110,8 @@ print("")
 
 time1 = time.time()
 
-best_model_dt = DecisionTreeClassifier(**best_param)
-best_model_dt.fit(X_test, y_test)
-
+best_model_dt = DecisionTreeClassifier(**best_model.get_params())
+best_model_dt.fit(X_train, y_train)
 y_pred = best_model_dt.predict(X_test)
 
 time2 = time.time()
@@ -128,7 +127,7 @@ print("\nClassification report:\n\n", classification_report(y_test, y_pred))
 
 #-----
 
-cross_val_scores = cross_val_score(best_model, X_train, y_train, scoring='accuracy', cv=5)
+cross_val_scores = cross_val_score(best_model, X_train, y_train, scoring='f1', cv=5)
 average_precision = cross_val_scores.mean()
 
 best_model.fit(X_train, y_train)
@@ -145,7 +144,7 @@ sns.heatmap(precision_scores.reshape(len(param_grid['max_depth']), -1),
             yticklabels=param_grid['max_depth'])
 plt.xlabel('min_samples_leaf')
 plt.ylabel('max_depth')
-plt.title('Grid Search Precision Scores')
+plt.title('Grid Search f1 Scores')
 plt.show()
 
 
