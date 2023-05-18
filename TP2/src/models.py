355a4cd88faf2c4584915b6e4bdd796e0686b1cd
Mónica Araújo <up202005209@up.pt>
2023-05-18 01:13:16 +0100

    improve accuracy of Decision Tree

--- TP2/src/models.py
+++ TP2/src/models.py
@@ -1,6 +1,6 @@
 import time
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
 from imblearn.over_sampling import SMOTE
 from sklearn import metrics
 from sklearn.tree import DecisionTreeClassifier
@@ -11,8 +11,11 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-dataset = pd.read_csv('../dataset/Heart Disease - Processed.csv')
+dataset = pd.read_csv('../dataset/Heart_Disease_Processed.csv')
 
 # Select features and target variable
 features = dataset.columns.tolist()[1:]
@@ -32,7 +35,9 @@ X_train, y_train = smote.fit_resample(X_train, y_train)
 time1 = time.time()
 
 model_dt = DecisionTreeClassifier()
-model_dt = model_dt.fit(X_train, y_train)
+model_dt.fit(X_train, y_train)
+model_dt.score(X_test, y_test)
+
 y_pred = model_dt.predict(X_test)
 
 time2 = time.time()
@@ -44,7 +49,106 @@ print("\nAccuracy: ", model_dt.score(X_test, y_test))
 print("Precision: ", metrics.precision_score(y_test, y_pred))
 print("Recall: ", metrics.recall_score(y_test, y_pred))
 print("F1 Score: ", metrics.f1_score(y_test, y_pred))
-print("\nConfusion Matrix\n\n", pd.crosstab(y_test, y_pred, rownames=['Actual'], colnames=['Predicted']))
+print("\nConfusion Matrix:\n\n", pd.crosstab(y_test, y_pred, rownames=['Actual'], colnames=['Predicted']))
+print("\nClassification report:\n\n", classification_report(y_test, y_pred))
+
+
+
+# --------------------- DECISION TREE Mod --------------------- #
+
+time1 = time.time()
+
+mod_model_dt = DecisionTreeClassifier(splitter="random", max_depth=200, min_samples_leaf=10, max_features="log2", criterion="entropy")
+mod_model_dt.fit(X_train, y_train)
+mod_model_dt.score(X_test, y_test)
+
+y_pred = mod_model_dt.predict(X_test)
+
+time2 = time.time()
+
+# Evaluate the model
+print("\nDecision Tree Mod ---------------------\n")
+print("Time spent: ", time2 - time1)
+print("\nAccuracy: ", mod_model_dt.score(X_test, y_test))
+print("Precision: ", metrics.precision_score(y_test, y_pred))
+print("Recall: ", metrics.recall_score(y_test, y_pred))
+print("F1 Score: ", metrics.f1_score(y_test, y_pred))
+print("\nConfusion Matrix:\n\n", pd.crosstab(y_test, y_pred, rownames=['Actual'], colnames=['Predicted']))
+print("\nClassification report:\n\n", classification_report(y_test, y_pred))
+
+
+# --------------------- DECISION TREE grid search --------------------- #
+
+print("\nDecision Tree grid Search ---------------------\n")
+
+grid_model_dt = DecisionTreeClassifier()
+param_grid = {
+    'splitter': ['best', 'random'],
+    'max_depth': [None, 10, 50, 100, 200],
+    'min_samples_leaf': [1, 5, 10, 20],
+    'max_features': [1, 5, 10, 'sqrt', 'log2'],
+    'criterion': ['gini', 'entropy']
+}
+grid_search = GridSearchCV(
+    estimator=grid_model_dt,
+    param_grid=param_grid,
+    scoring='accuracy',
+    cv=5,
+    n_jobs=-1
+)
+grid_search.fit(X_train, y_train)
+
+best_score = grid_search.best_score_
+best_param = grid_search.best_params_
+best_model = grid_search.best_estimator_
+
+# Evaluate the best model individually
+print("\nbest param: ", best_param)
+print("best model: ", best_model)
+print("best score: ", best_score)
+print("")
+
+time1 = time.time()
+
+best_model_dt = DecisionTreeClassifier(**best_param)
+best_model_dt.fit(X_test, y_test)
+
+y_pred = best_model_dt.predict(X_test)
+
+time2 = time.time()
+
+print("\nDecision Tree with best parameter ---------------------\n")
+print("Time spent: ", time2 - time1)
+print("\nAccuracy: ", best_model_dt.score(X_test, y_test))
+print("Precision: ", metrics.precision_score(y_test, y_pred))
+print("Recall: ", metrics.recall_score(y_test, y_pred))
+print("F1 Score: ", metrics.f1_score(y_test, y_pred))
+print("\nConfusion Matrix:\n\n", pd.crosstab(y_test, y_pred, rownames=['Actual'], colnames=['Predicted']))
+print("\nClassification report:\n\n", classification_report(y_test, y_pred))
+
+#-----
+
+cross_val_scores = cross_val_score(best_model, X_train, y_train, scoring='accuracy', cv=5)
+average_precision = cross_val_scores.mean()
+
+best_model.fit(X_train, y_train)
+y_pred = best_model.predict(X_test)
+
+#show
+results = grid_search.cv_results_
+precision_scores = results['mean_test_score']
+
+plt.figure(figsize=(10, 6))
+sns.heatmap(precision_scores.reshape(len(param_grid['max_depth']), -1), 
+            annot=True, fmt='.4f', cmap='YlGnBu',
+            xticklabels=param_grid['min_samples_leaf'],
+            yticklabels=param_grid['max_depth'])
+plt.xlabel('min_samples_leaf')
+plt.ylabel('max_depth')
+plt.title('Grid Search Precision Scores')
+plt.show()
+
+
 
 # --------------------- NEURAL NETWORK --------------------- #
 
